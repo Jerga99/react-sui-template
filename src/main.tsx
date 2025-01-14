04a0557af9c08ecee47250819ff09f8f135a200d
Filip Jerga <jerga99@gmail.com>
2025-01-14 22:21:39 +0000

    Double quotes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import '@mysten/dapp-kit/dist/index.css';
-import App from './App.tsx'
-import { ThemeProvider } from './providers/theme/ThemeProvider.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import "@mysten/dapp-kit/dist/index.css";
+import App from "./App.tsx"
+import { ThemeProvider } from "./providers/theme/ThemeProvider.tsx"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
-import { networkConfig } from './config/networkConfig.ts'
+import { networkConfig } from "./config/networkConfig.ts"
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <SuiClientProvider defaultNetwork="testnet" networks={networkConfig}>
